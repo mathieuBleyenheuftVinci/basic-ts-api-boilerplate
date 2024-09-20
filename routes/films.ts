2b3240d3432ad055394cc6d476cfbebf8ebd3aee
mathieuBleyenheuftVinci <165761155+mathieuBleyenheuftVinci@users.noreply.github.com>
2024-09-20 09:46:36 +0200

    Ex1.1

---
+++ routes/films.ts
@@ -0,0 +1,40 @@
+import { Router } from "express";
+import {Film} from "../types";
+
+const router = Router();
+
+const films: Film[] = [
+    {
+      id: 1,
+      title: "Inception",
+      director: "Christopher Nolan",
+      duration: 148,
+      budget: 160, // en millions
+      description: "Un film qui explore les rêves et la réalité.",
+      imageURL: "https://example.com/inception.jpg"
+    },
+    {
+      id: 2,
+      title: "Interstellar",
+      director: "Christopher Nolan",
+      duration: 169,
+      budget: 165, // en millions
+      description: "Un voyage épique à travers l'espace et le temps.",
+      imageURL: "https://example.com/interstellar.jpg"
+    },
+    {
+      id: 3,
+      title: "The Matrix",
+      director: "The Wachowskis",
+      duration: 136,
+      budget: 63, // en millions
+      description: "Un hacker découvre la réalité simulée du monde.",
+      imageURL: "https://example.com/matrix.jpg"
+    }
+  ];
+
+  router.get("/", (_req, res) => {
+    res.json({ films });
+  });
+
+export default router;
